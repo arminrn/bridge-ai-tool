420b4d07606c1577ffadcce363df4be60a19cca8
Armin Rashidi Nasab <94007036+arminrn@users.noreply.github.com>
2026-03-25 17:05:08 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -23,7 +23,7 @@ if uploaded_file is not None:
                 }
 
                 response = requests.post(
-                    "http://localhost:8000/analyze/",
+                    ""https://bridge-ai-tool.onrender.com/analyze/"",
                     files=files
                 )
 
